4bdad8cd3b74ee6b25bf5d809da5e6a8c611c790
Mert Gulcan <mertgulcan.92@gmail.com>
2022-02-04 11:17:08 +0300

    fix

--- MarsRovers/Program.cs
+++ MarsRovers/Program.cs
@@ -11,41 +11,52 @@ namespace MarsRovers
 
             AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(ErrorHandler);
 
+            
             Console.WriteLine("Enter the right upper coordinates of planet with space (ex: 5 5)");
+            
             string tmpPlanetCoordinates = Console.ReadLine();
 
             var planetCoordinates= tmpPlanetCoordinates.Split(' ');
+
             Planet planet = new Planet(Convert.ToInt32(planetCoordinates[0]),Convert.ToInt32(planetCoordinates[1]));
 
+            
             Console.WriteLine("Enter the 1.Rover position with space (ex: 1 2 N)");
 
             string tmpPosition = Console.ReadLine().ToUpper();
+            
             var roverPosition = tmpPosition.Split(' ');
 
             Position position = new Position(Convert.ToInt32(roverPosition[0]), Convert.ToInt32(roverPosition[1]), Enum.Parse<Facing>(roverPosition[2]));
 
             Rover rover = new Rover(planet, position);
 
+            
             Console.WriteLine("Enter the 1.Rover directive line command(ex: LMLMLMLMM)");
 
             string roverDirectiveCommand = Console.ReadLine().ToUpper();
 
+            
             Console.WriteLine("Enter the 2.Rover position with space (ex: 3 3 E)");
 
             string tmp2Position = Console.ReadLine().ToUpper();
+            
             var rover2Position = tmp2Position.Split(' ');
 
             Position position2 = new Position(Convert.ToInt32(rover2Position[0]), Convert.ToInt32(rover2Position[1]), Enum.Parse<Facing>(rover2Position[2]));
 
             Rover rover2 = new Rover(planet, position2);
 
+            
             Console.WriteLine("Enter the 2.Rover directive line command(ex: MMRMMRMRRM)");
 
             string rover2DirectiveCommand = Console.ReadLine().ToUpper();
 
+            
             rover.GetDirectivesWithLineCommand(roverDirectiveCommand);
 
             Console.WriteLine(rover.CurrentPosition.ToString());
+
             
             rover2.GetDirectivesWithLineCommand(rover2DirectiveCommand);
             Console.WriteLine(rover2.CurrentPosition.ToString());
